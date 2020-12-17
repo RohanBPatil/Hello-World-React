bf467b998025402b04a782a6feea5a99eae5a391
RohanBPatil <rohanpatil14218@gmail.com>
2020-12-17 16:03:40 +0530

    Added : Web page to display hello message and demonstrated onClick and onChange data binding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  url = 'https://www.bridgelabz.com/'
+  constructor(){
+    super()
+    this.state = {
+      userName : '',
+      nameError: ''
+    }
+  }
+
+  // onclick function
+  onClick = ($event) => {
+    console.log("save button is clicked!!", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onNameChange = (event) => {
+    console.log("Value is ", event.target.value);
+    const nameRegex = RegExp('^[A-Z]{1}[A-Za-z ]{1,}$');
+    // set the title using setState method
+    this.setState({userName : event.target.value});
+    if(nameRegex.test(event.target.value)){
+      this.setState({nameError: ''})
+    } else{
+      this.setState({nameError: 'Name is incorrect'})
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <div>
+          <h1>Hello {this.state.userName} from Rohan</h1>
+            <img src={logo} onClick = {this.onClick}
+              alt="The bridgelabz logo: A bridge to employment" />
+        </div>
+        <div className="text-box">
+          <input onChange = {this.onNameChange} />
+          <span className="error-output">{this.state.nameError}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
